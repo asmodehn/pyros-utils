516a38394f099923530339b60bdc5f5d7e3a209e
AlexV <asmodehn@gmail.com>
2016-05-31 22:24:44 +0900

    fixing __init__ imports. added travis.yml.

--- pyros_utils/__init__.py
+++ pyros_utils/__init__.py
@@ -3,13 +3,8 @@ from __future__ import absolute_import
 
 from ._version import __version__
 
-import logging
-import os
-import pkgutil
-import types
-
-import sys
-
+from .ros_utils import get_master, get_ros_home
+from .rostest_nose import rostest_or_nose_main
 
 __all__ = [
     'get_master',
