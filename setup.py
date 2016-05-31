6162bf147e121ed6f0e5f452813c49b9df19009b
AlexV <asmodehn@gmail.com>
2016-05-31 22:48:07 +0900

    fixing cmakelists and setup.py. removed requirements.txt

--- setup.py
+++ setup.py
@@ -2,32 +2,30 @@
 # using setuptools : http://pythonhosted.org/setuptools/
 from setuptools import setup
 
-with open('pyros_setup/_version.py') as vf:
+with open('pyros_utils/_version.py') as vf:
     exec(vf.read())
 
-setup(name='pyros_setup',
+setup(name='pyros_utils',
     version='0.1.0',
-    description='Toolsuite for running ROS environments directly from python code, without any specific requirements outside of usual python',
-    url='http://github.com/asmodehn/pyros-setup',
+    description='This is a ROS package, providing useful ROS addons for pyros.',
+    url='http://github.com/asmodehn/pyros-utils',
     author='AlexV',
     author_email='asmodehn@gmail.com',
     license='BSD',
     packages=[
-        'pyros_setup',
-        'pyros_setup.config',
-        'pyros_setup.tests',
+        'pyros_utils',
+        'pyros_utils.tests',
     ],
     entry_points={
         'console_scripts': [
-            'pyros_setup = pyros_setup.__main__:nosemain'
+            'pyros_utils = pyros_utils.__main__:nosemain'
         ]
     },
     # this is better than using package data ( since behavior is a bit different from distutils... )
     include_package_data=True,  # use MANIFEST.in during install.
     install_requires=[
-        #'catkin_pkg',  # not needed here since this version should not look for package.xml
+        'catkin_pkg',  # not needed here since this version should not look for package.xml
         'six',
-        'importlib'
     ],
     test_suite="nose.collector",
     tests_require=[
